d607a4627aa81b4e7748a523972ae6307f2e4ac2
torpedoallen <torpedoallen@gmail.com>
2015-10-26 08:36:14 +0000

    fix encoding

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 # coding=utf8
 
 from flask import Flask, request
+from wechat_sdk import WechatBasic
+
 app = Flask(__name__)
 
-
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    echostr = request.args.get('echostr', '')
-    return echostr
+    # 下面这些变量均假设已由 Request 中提取完毕
+    token = 'homie'  # 你的微信 Token
+    signature = request.args.get('signature', '')
+    timestamp = request.args.get('timestamp', '')
+    nonce = request.args.get('nonce', '')
+
+    # 实例化 wechat
+    wechat = WechatBasic(token=token)
+
+    if not wechat.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
+        return 'fail'
+
+    # 对签名进行校验
+    echostr = request.args.get('echostr')
+    if echostr:
+        return echostr
+
+    wechat.parse_data(request.data)
+    message = wechat.get_message()
+    if message.type == 'text':
+        response = wechat.response_text(u'^_^')
+    elif message.type == 'image':
+        response = wechat.response_text(u'图片')
+    else:
+        response = wechat.response_text(u'未知')
+    return response
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9998)
