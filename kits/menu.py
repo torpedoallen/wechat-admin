95006b69ac865f3545afd4badb49e6eb3d52f43b
torpedoallen <torpedoallen@gmail.com>
2015-10-28 05:33:45 +0000

    hack

--- kits/menu.py
+++ kits/menu.py
@@ -9,6 +9,8 @@ class WechatMenuAdapter(object):
 
     @classmethod
     def get_menus(cls):
-        token = config.token
-        wechat = WechatBasic(token=token)
-        return wechat.get_menu()
+        #wechat = WechatBasic(appid=config.app_id, appsecret=config.secret)
+        #d = wechat.get_access_token()
+        #wechat = WechatBasic(appid=config.app_id, appsecret=config.secret, access_token=d['access_token'], access_token_expires_at=d['access_token_expires_at'])
+        #return wechat.get_menu()
+        return '''{"menu":{"button":[{"type":"click","name":"今日歌曲","key":"V1001_TODAY_MUSIC","sub_button":[]},{"type":"click","name":"歌手简介","key":"V1001_TODAY_SINGER","sub_button":[]},{"name":"菜单","sub_button":[{"type":"view","name":"搜索","url":"http://www.soso.com/","sub_button":[]},{"type":"view","name":"视频","url":"http://v.qq.com/","sub_button":[]},{"type":"click","name":"赞一下我们","key":"V1001_GOOD","sub_button":[]}]}]}}'''
