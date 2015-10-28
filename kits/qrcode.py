1c8fcc21a6fe5e8cb28b631fe1a42d304fb8e059
torpedoallen <torpedoallen@gmail.com>
2015-10-28 06:24:41 +0000

    add wrapper

--- kits/qrcode.py
+++ kits/qrcode.py
@@ -3,14 +3,19 @@
 
 import config
 from wechat_sdk import WechatBasic
+import wechat_wrapper as _wechat
 
 
 class WechatQrcodeAdapter(object):
 
-
     @classmethod
     def create_qrcode(cls, **kw):
-        wechat = WechatBasic(appid=config.app_id, appsecret=config.secret)
+        token, expired_at = _wechat.get_access_token()
+        wechat = WechatBasic(
+            appid=config.app_id,
+            appsecret=config.secret,
+            access_token=token,
+            access_token_expires_at=expired_at)
         payload = {
             "action_name": "QR_LIMIT_STR_SCENE",
             "action_info": {"scene": {"scene_str": "张三"}}}
@@ -18,6 +23,6 @@ class WechatQrcodeAdapter(object):
 
     @classmethod
     def show_qrcode(cls, ticket):
-        wechat = WechatBasic(appid=config.app_id, appsecret=config.secret)
+        #wechat = WechatBasic(appid=config.app_id, appsecret=config.secret)
         #return wechat.show_qrcode(ticket)
         return '''{"ticket":"gQH47joAAAAAAAAAASxodHRwOi8vd2VpeGluLnFxLmNvbS9xL2taZ2Z3TVRtNzJXV1Brb3ZhYmJJAAIEZ23sUwMEmm3sUw==","expire_seconds":60,"url":"http:\/\/weixin.qq.com\/q\/kZgfwMTm72WWPkovabbI"}'''
---
+++ wechat_wrapper.py
@@ -0,0 +1,79 @@
+# coding=utf8
+
+
+import time
+import random
+import datetime
+import config
+from wechat_sdk import WechatBasic
+from werkzeug.contrib.cache import SimpleCache
+
+cache = SimpleCache()
+
+TOKEN_KEY = 'wx_at'
+TOKEN_EXPIRED_AT_KEY = 'wx_atet'
+TICKET_KEY = 'wx_tk'
+TICKET_EXPIRED_AT_KEY = 'wx_tket'
+
+
+def get_access_token():
+    token = cache.get(TOKEN_KEY)
+    token_expired_at = cache.get(TOKEN_EXPIRED_AT_KEY)
+    if token:
+        return token, token_expired_at
+    b = WechatBasic(appid=config.app_id, appsecret=config.secret)
+    print 'get_access_token at:', datetime.datetime.now()
+    d = b.get_access_token()
+    token = d['access_token']
+    expired_at = d['access_token_expires_at']
+    cache.set(TOKEN_KEY, token, (expired_at - time.time())*60)
+    cache.set(TOKEN_EXPIRED_AT_KEY, expired_at, (expired_at - time.time())*60)
+    return token, expired_at
+
+
+def get_jsapi_ticket():
+    ticket = cache.get(TICKET_KEY)
+    expired_at = cache.get(TICKET_EXPIRED_AT_KEY)
+    if ticket:
+        return ticket, expired_at
+
+    token, expired_at = get_access_token()
+    b = WechatBasic(
+        appid=config.app_id,
+        appsecret=config.secret,
+        access_token=token,
+        access_token_expires_at=expired_at)
+
+    print 'get_ticket at:', datetime.datetime.now()
+    d = b.get_jsapi_ticket()
+
+    ticket = d['jsapi_ticket']
+    expired_at = d['jsapi_ticket_expires_at']
+
+    cache.set(TICKET_KEY, ticket, (expired_at - time.time())*60)
+    cache.set(TICKET_EXPIRED_AT_KEY, expired_at, (expired_at - time.time())*60)
+    return ticket, expired_at
+
+
+def generate_jsapi_signature(url):
+
+    ticket, expired_at = get_jsapi_ticket()
+
+    b = WechatBasic(
+        appid=config.app_id,
+        appsecret=config.secret,
+        jsapi_ticket=ticket,
+        jsapi_ticket_expires_at=expired_at)
+
+    timestamp = int(round(time.time()))
+    nonce = random.randint(10000000, 99999999)
+    print 'generate signature at:', datetime.datetime.now()
+    signature = b.generate_jsapi_signature(
+        timestamp, nonce, url, jsapi_ticket=ticket)
+
+    return {
+        'appId': config.app_id,
+        'timestamp': timestamp,
+        'nonceStr': nonce,
+        'signature': signature,
+    }
