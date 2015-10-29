2e99065a105c08681335710fb6c6d1e87123ce98
torpedoallen <torpedoallen@gmail.com>
2015-10-29 05:01:05 +0000

    add event part

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import request, Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from wechat_sdk import WechatBasic
+from wechat_sdk.messages import EventMessage
+
 from kits.menu import WechatMenuAdapter
 from kits.qrcode import WechatQrcodeAdapter
 
@@ -77,6 +79,25 @@ def index():
         response = wechat.response_text(settings.auto_replay_text)
     elif message.type == 'image':
         response = wechat.response_text(u'图片')
+    elif isinstance(message, EventMessage):  # 事件信息
+        if message.type == 'subscribe':  # 关注事件(包括普通关注事件和扫描二维码造成的关注事件)
+            if message.key and message.ticket:  # 如果 key 和 ticket 均不为空，则是扫描二维码造成的关注事件
+                response = wechat.response_text(content=u'用户尚未关注时的二维码扫描关注事件')
+            else:
+                response = wechat.response_text(content=u'普通关注事件')
+        elif message.type == 'unsubscribe':
+            response = wechat.response_text(content=u'取消关注事件')
+        elif message.type == 'scan':
+            response = wechat.response_text(content=u'用户已关注时的二维码扫描事件')
+        elif message.type == 'location':
+            response = wechat.response_text(content=u'上报地理位置事件')
+        elif message.type == 'click':
+            print message.key
+            response = wechat.response_text(content=u'自定义菜单点击事件')
+        elif message.type == 'view':
+            response = wechat.response_text(content=u'自定义菜单跳转链接事件')
+        elif message.type == 'templatesendjobfinish':
+            response = wechat.response_text(content=u'模板消息事件')
     else:
         response = wechat.response_text(u'未知')
     return response
