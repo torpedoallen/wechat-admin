3a2efc4e21776b30b535aaf4da33fb3416d0dad4
torpedoallen <torpedoallen@gmail.com>
2015-11-02 11:38:08 +0000

    bugfix

--- scripts/initdb.py
+++ scripts/initdb.py
@@ -1,14 +1,11 @@
 # coding=utf8
 
-import os
 import sys
 from os.path import abspath, dirname
 
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
-from wechat_admin import db
-
-print db
+from app import db
 
 db.create_all()
 db.session.commit()
